88b438acf20a15233570b20633e56895fb8151d1
aorko01 <aorko321@gmail.com>
2025-05-20 22:24:52 +0600

    practised

---
+++ topics/binary_search/B_Closest_to_the_Left.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <type_traits>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):" , dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+bool f(ll x, ll target) {
+    return x <= target;
+}
+
+ll last_true(ll lo, ll hi, ll target, vector<ll>& arr) {
+	for (ll dif = hi - lo; dif > 0; dif /= 2) {
+		while (lo + dif <= hi && f(arr[lo + dif], target)) { lo += dif; }
+	}
+	return lo;
+}
+
+
+void solve() {
+    ll n,k;
+    cin>>n>>k;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for(ll i = 0; i < k; i++) {
+        ll x;
+        cin >> x;
+        ll ans = last_true(-1, n - 1, x, a);
+        // if (ans == -1) {
+        //     cout << "0" << endl;  
+        // } else {
+            cout << ans + 1 << endl;
+        // }
+    }
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    ll tc = 1;
+    // cin >> tc;
+    for (ll t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+}
